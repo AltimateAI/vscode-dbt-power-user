5a66bfa1ddd7d8fd2779601810c803f1616418b0
Shao Yuan Weng <shaoyuan.weng@gmail.com>
2020-08-25 14:05:55 +0200

    fixed source name lookup

--- src/definition_provider/sourceDefinitionProvider.ts
+++ src/definition_provider/sourceDefinitionProvider.ts
@@ -7,7 +7,8 @@ import { isEnclosedWithinCodeBlock } from "../utils";
 export class SourceDefinitionProvider implements DefinitionProvider {
   private sourceMetaMap: SourceMetaMap = new Map();
   private static readonly IS_SOURCE = /(source)[^}]*/;
-  private static readonly GET_SOURCE_NAME = /(?!['"])(\w+)(?=['"])/ig;
+  private static readonly HAS_SOURCE_NAME = /(?!['"])(\w+)(?=['"])/;
+  private static readonly GET_SOURCE_INFO = /(?!['"])(\w+)(?=['"])/g;
 
   provideDefinition(
     document: TextDocument,
@@ -19,15 +20,27 @@ export class SourceDefinitionProvider implements DefinitionProvider {
       const range = document.getWordRangeAtPosition(position, SourceDefinitionProvider.IS_SOURCE);
       const word = document.getText(range);
 
-      if (range && word !== undefined && hover !== "source"
-        && isEnclosedWithinCodeBlock(document, range)) {
-        const source = word.match(SourceDefinitionProvider.GET_SOURCE_NAME);
-        if (source) {
-          const definition = this.getSourceDefinition(source[0]);
-          resolve(definition);
-          return;
-        }
+      const linePrefix = document
+        .lineAt(position)
+        .text.substr(0, position.character);
+
+      if (!isEnclosedWithinCodeBlock(document, position) ||
+        !linePrefix.includes('source') ||
+        hover === 'source') { return undefined; }
+
+      const source = word.match(SourceDefinitionProvider.GET_SOURCE_INFO);
+      if (source === null || source === undefined) {
+        return undefined;
       }
+
+      const sourceInfo = linePrefix.match(SourceDefinitionProvider.HAS_SOURCE_NAME) ? this.getTableName(source) : this.getSourceName(source);
+
+      if (sourceInfo) {
+        const definition = this.getSourceDefinition(sourceInfo.sourceName, sourceInfo.lookupItem);
+        resolve(definition);
+        return;
+      }
+      
       reject();
     });
   }
@@ -36,18 +49,32 @@ export class SourceDefinitionProvider implements DefinitionProvider {
     this.sourceMetaMap = event.sourceMetaMap;
   }
 
-  private getSourceDefinition(name: string): Definition | undefined {
-    const location = this.sourceMetaMap.get(name);
+  private getSourceName(source: RegExpMatchArray) {
+    return {
+      sourceName: source[0],
+      lookupItem: source[0]
+    };
+  }
+
+  private getTableName(source: RegExpMatchArray) {
+    return {
+      sourceName: source[0],
+      lookupItem: source[1]
+    };
+  }
+
+  private getSourceDefinition(sourceName: string, lookupItem: string): Definition | undefined {
+    const location = this.sourceMetaMap.get(sourceName);
     if (location) {
       const sourceFile: string = readFileSync(location.path).toString("utf8");
       const sourceFileLines = sourceFile.split("\n");
 
       for (let index = 0; index < sourceFileLines.length; index++) {
         const currentLine = sourceFileLines[index];
-        if (currentLine.includes(name)) {
+        if (currentLine.includes(lookupItem)) {
           return new Location(
             Uri.file(location.path),
-            new Position(index, currentLine.indexOf(name))
+            new Position(index, currentLine.indexOf(lookupItem))
           );
         }
       }
