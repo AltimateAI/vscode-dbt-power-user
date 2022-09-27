8df96d73b22d08bb820141e0fea6d989fcf37833
Michiel De Smet <mdesmet@gmail.com>
2022-09-27 22:38:51 +0200

    Add compilation on change

--- src/content_provider/sqlPreviewContentProvider.ts
+++ src/content_provider/sqlPreviewContentProvider.ts
@@ -1,34 +1,59 @@
 import {
   Event,
   EventEmitter,
+  FileSystemWatcher,
   TextDocumentContentProvider,
   Disposable,
   Uri,
+  workspace,
+  RelativePattern
 } from 'vscode';
 import { readFileSync } from "fs";
 import { DBTProjectContainer } from '../manifest/dbtProjectContainer';
-import { provideSingleton } from '../utils';
+import { debounce, provideSingleton } from '../utils';
 
 @provideSingleton(SqlPreviewContentProvider)
 export class SqlPreviewContentProvider implements TextDocumentContentProvider, Disposable {
   static readonly SCHEME = 'query-preview';
 
-  public onDidChangeEmitter = new EventEmitter<Uri>();
+  private _onDidChange = new EventEmitter<Uri>();
+  private compilationDocs = new Map<string, Uri>();
+  private subscriptions: Disposable;
+  private watchers: FileSystemWatcher[] = [];
   
   constructor(
     private dbtProjectContainer: DBTProjectContainer
-  ) {}
+  ) {
+    this.subscriptions = workspace.onDidCloseTextDocument(compilationDoc => this.compilationDocs.delete(compilationDoc.uri.toString()));
+  }
 
   dispose(): void {
-    this.onDidChangeEmitter.dispose();
+    this._onDidChange.dispose();
+    this.subscriptions.dispose();
+    while (this.watchers.length) {
+      const x = this.watchers.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
   }
 
   get onDidChange(): Event<Uri> {
-    return this.onDidChangeEmitter.event;
+    return this._onDidChange.event;
   }
 
   provideTextDocumentContent(uri: Uri): string | Thenable<string> {
-    // TODO: implement recompilation when file is changed.
+    if (this.compilationDocs.get(uri.toString()) === undefined) {
+      this.compilationDocs.set(uri.toString(), uri);
+      const watcher = workspace.createFileSystemWatcher(new RelativePattern(uri, '*'));
+      this.watchers.push(watcher);
+      watcher.onDidChange(debounce(() => this._onDidChange.fire(uri), 300));
+      // TODO: onDelete? onCreate?
+    }
+    return this.requestCompilation(uri);
+  }
+
+  private requestCompilation(uri: Uri) {
     const fsPath = decodeURI(uri.path);
     try {
       const query = readFileSync(fsPath, "utf8");
