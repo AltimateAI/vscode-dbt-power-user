d748b6db5880d0faef317ddfbcd950b19ae7450a
Michiel De Smet <mdesmet@gmail.com>
2022-11-20 00:50:20 +0100

    fixup! Support docs definition provider

---
+++ src/manifest/parsers/docParser.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from "fs";
+import { provide } from "inversify-binding-decorators";
+import * as path from "path";
+import { DBTTerminal } from "../../dbt_client/dbtTerminal";
+import { DocMetaMap } from "../../domain";
+
+@provide(DocParser)
+export class DocParser {
+  constructor(private terminal: DBTTerminal) {}
+
+  createDocMetaMap(docs: any[], projectName: string): Promise<DocMetaMap> {
+    return new Promise((resolve) => {
+      const docMetaMap: DocMetaMap = new Map();
+      if (docs === null || docs === undefined) {
+        resolve(docMetaMap);
+      }
+      Object.values(docs).forEach(
+        ({ package_name, name, root_path, original_file_path }) => {
+          const packageName = package_name;
+          const docName =
+            packageName === projectName ? name : `${packageName}.${name}`;
+          const fullPath = path.join(root_path, original_file_path);
+          try {
+            const docFile: string = readFileSync(fullPath).toString("utf8");
+            const macroFileLines = docFile.split("\n");
+            for (let index = 0; index < macroFileLines.length; index++) {
+              const currentLine = macroFileLines[index];
+              if (currentLine.match(new RegExp(`docs\\s${name}`))) {
+                docMetaMap.set(docName, {
+                  path: fullPath,
+                  line: index,
+                  character: currentLine.indexOf(name),
+                });
+                break;
+              }
+            }
+          } catch (error) {
+            console.log(
+              `File not found at '${fullPath}', project may need to be recompiled.`,
+              error
+            );
+            this.terminal.log(
+              `File not found at '${fullPath}', probably compiled is outdated. ${error}`
+            );
+          }
+        }
+      );
+      resolve(docMetaMap);
+    });
+  }
+}
