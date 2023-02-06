7f421723463f5724aa94685294645973ed7d1e10
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-02-06 15:34:22 -0700

    fix #305: fix typo in environment error message

--- src/manifest/dbtProject.ts
+++ src/manifest/dbtProject.ts
@@ -253,7 +253,7 @@ export class DBTProject implements Disposable {
   private async rebuildManifest() {
     if (!this.pythonBridgeInitialized) {
       window.showErrorMessage(
-        "The dbt manifest can't be rebuild right now as the Python envirnoment has not yet been initialized, please try again later."
+        "The dbt manifest can't be rebuild right now as the Python environment has not yet been initialized, please try again later."
       );
       return;
     }
