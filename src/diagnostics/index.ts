f7a6f812e87f01b0a6247a5070df2151bb7e414c
Michiel De Smet <mdesmet@gmail.com>
2023-09-05 21:08:02 +0200

    Add diagnostics for Python bridge

--- src/diagnostics/index.ts
+++ src/diagnostics/index.ts
@@ -6,7 +6,11 @@ export class DbtPowerUserDiagnostics implements Disposable {
   public readonly rebuildManifestDiagnostics =
     languages.createDiagnosticCollection("dbt");
 
+  public readonly pythonBridgeDiagnostics =
+    languages.createDiagnosticCollection("dbt");
+
   dispose() {
     this.rebuildManifestDiagnostics.dispose();
+    this.pythonBridgeDiagnostics.dispose();
   }
 }
