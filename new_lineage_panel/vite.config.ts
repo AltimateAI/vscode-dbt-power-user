8d5956b159680e27b85c7b4e716eacb857d33453
Aditya <aditya.gajbhiye.20@gmail.com>
2023-09-21 21:42:14 +0100

    WIP

--- new_lineage_panel/vite.config.ts
+++ new_lineage_panel/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react()],
   base: "__ROOT__",
   build: {
     rollupOptions: {
