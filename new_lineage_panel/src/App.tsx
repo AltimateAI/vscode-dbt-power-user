49bb13c0208aa89fbee28fba1f5b3af23d1876e3
Aditya <aditya.gajbhiye.20@gmail.com>
2023-09-22 02:10:51 +0100

    feat: added minimap

--- new_lineage_panel/src/App.tsx
+++ new_lineage_panel/src/App.tsx
@@ -7,6 +7,7 @@ import {
   useState,
 } from "react";
 import ReactFlow, {
+  MiniMap,
   Background,
   Controls,
   Edge,
@@ -235,6 +236,7 @@ function App() {
             >
               <Background />
               <Controls />
+              <MiniMap />
             </ReactFlow>
           </div>
           <SidebarModal
