3e09ae7807b2fc97f2c67ae270722c84f69b76c2
Aditya <aditya.gajbhiye.20@gmail.com>
2023-10-03 18:31:20 +0100

    fix: remove styling from input

--- new_lineage_panel/src/Form.tsx
+++ new_lineage_panel/src/Form.tsx
@@ -1,16 +1,5 @@
 import { Input, InputProps } from "reactstrap";
-import { isDarkMode } from "./App";
 
 export function CustomInput(props: InputProps) {
-  return (
-    <Input
-      className="custom-input"
-      style={{
-        backgroundColor: isDarkMode ? "rgba(131, 144, 163, 0.2)" : "#ffffff",
-        color: isDarkMode ? "#ffffff" : "#212529",
-        borderColor: isDarkMode ? "rgba(131, 144, 163, 0.1)" : "#dee2e6",
-      }}
-      {...props}
-    />
-  );
+  return <Input className="custom-input" {...props} />;
 }
