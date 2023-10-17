b80413c85e101d19b3c0ed617769f64068c2942d
shardul <shardul@altimate.ai>
2023-10-17 07:00:47 -0700

    feat: Plain sql to model (#633)
    * revert changes to doc panel (+3 squashed commits)
    Squashed commits:
    [b5dcdfd] limit to current project, use compiled sql to allow partially migrated sqls
    [392d7fb] add a new provider to associate models.
    [0079a10] WIP: hacking doc panel for now
    
    * change icon to avoid confusion
    
    * add db/schema info when parsing sourcemetadata. also send sources to sqltomodel endpoint
    
    * add source name as well since that is what appears in the reference
    
    * conditionally show the new option if config is set right.
    
    * add errors to surface to users.
    
    * fix err message
    
    * add telemetry
    
    * change verbiage
    
    * move to end of nav icons
    
    * Improve context menu string
    
    ---------
    
    Co-authored-by: Michiel De Smet <mdesmet@gmail.com>

---
+++ src/commands/sqlToModel.ts
@@ -0,0 +1,112 @@
+import { AltimateRequest } from "../altimate";
+import { NodeMetaData, SourceMetaData } from "../domain";
+import { DBTProjectContainer } from "../manifest/dbtProjectContainer";
+import {
+  ManifestCacheChangedEvent,
+  ManifestCacheProjectAddedEvent,
+} from "../manifest/event/manifestCacheChangedEvent";
+import { TelemetryService } from "../telemetry";
+import { provideSingleton } from "../utils";
+import { Position, Range, window } from "vscode";
+
+@provideSingleton(SqlToModel)
+export class SqlToModel {
+  private eventMap: Map<string, ManifestCacheProjectAddedEvent> = new Map();
+  constructor(
+    private dbtProjectContainer: DBTProjectContainer,
+    private telemetry: TelemetryService,
+    private altimate: AltimateRequest,
+  ) {
+    dbtProjectContainer.onManifestChanged((event) =>
+      this.onManifestCacheChanged(event),
+    );
+  }
+
+  private async onManifestCacheChanged(event: ManifestCacheChangedEvent) {
+    event.added?.forEach((added) => {
+      this.eventMap.set(added.projectRoot.fsPath, added);
+    });
+    event.removed?.forEach((removed) => {
+      this.eventMap.delete(removed.projectRoot.fsPath);
+    });
+  }
+
+  async getModelFromSql() {
+    this.telemetry.sendTelemetryEvent("sqlToModel");
+    if (!window.activeTextEditor) {
+      return;
+    }
+    const activedoc = window.activeTextEditor;
+    const currentFilePath = activedoc.document.uri;
+    const allmodels: NodeMetaData[] = [];
+    const project = this.dbtProjectContainer.findDBTProject(currentFilePath);
+    if (project === undefined) {
+      window.showErrorMessage(
+        "Could not find a dbt project. \
+      Please put the new model in a dbt project before converting to a model.",
+      );
+      this.telemetry.sendTelemetryError("sqlToModelNoProjectError");
+      return undefined;
+    }
+    const event = this.eventMap.get(project.projectRoot.fsPath);
+    if (event === undefined) {
+      window.showErrorMessage(
+        "Could not convert to model due to pending initiation, \
+      Please retry again.",
+      );
+      this.telemetry.sendTelemetryError("sqlToModelNoManifestError");
+      return undefined;
+    }
+    const rriter = event.nodeMetaMap.values();
+    for (const rrvalue of rriter) {
+      allmodels.push(rrvalue);
+    }
+
+    const allsources: SourceMetaData[] = [];
+    const srcriter = event.sourceMetaMap.values();
+    for (const srcvalue of srcriter) {
+      allsources.push(srcvalue);
+    }
+
+    const fileText = activedoc.document.getText();
+    const compiled_sql = await project.compileQuery(fileText);
+
+    const retobj = await this.altimate
+      .runModeller({
+        // if we can run this through compile sql, we can also do
+        // conversions that were half done. if it fails, just send the text as is.
+        sql: compiled_sql || fileText,
+        adapter: project.getAdapterType(),
+        models: allmodels,
+        sources: allsources,
+      })
+      .catch((err) => {
+        window.showErrorMessage(
+          "Could not convert sql to model. \
+        Encountered unknown error when converting sql to model.",
+        );
+        this.telemetry.sendTelemetryError("sqlToModelBackendError", err);
+        console.error(err);
+        return undefined;
+      });
+    // if somehow the response isnt there or got an error response
+    if (retobj === undefined || retobj.sql === undefined) {
+      window.showErrorMessage(
+        "Could not convert sql to model. \
+        Encountered unknown error when converting sql to model.",
+      );
+      this.telemetry.sendTelemetryError("sqlToModelEmptyBackendResponseError");
+      return undefined;
+    }
+
+    const startpos = new Position(0, 0);
+    const endpos = new Position(
+      activedoc.document.lineCount,
+      activedoc.document.lineAt(activedoc.document.lineCount - 1).text.length,
+    );
+    activedoc.edit((editBuilder) => {
+      editBuilder.replace(new Range(startpos, endpos), retobj.sql);
+    });
+    return;
+  }
+}
