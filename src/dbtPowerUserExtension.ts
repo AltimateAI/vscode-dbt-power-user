2fec78666b6c49949b18ec01bfa6b2c4c49efbd7
Michiel De Smet <mdesmet@gmail.com>
2023-11-09 11:28:10 +0100

    Fix showSetupWalkthrough

--- src/dbtPowerUserExtension.ts
+++ src/dbtPowerUserExtension.ts
@@ -20,11 +20,6 @@ import { TelemetryService } from "./telemetry";
 import { HoverProviders } from "./hover_provider";
 import { DbtPowerUserActionsCenter } from "./quickpick";
 
-enum PromptAnswer {
-  YES = "Yes",
-  IGNORE = "Ignore",
-}
-
 @provideSingleton(DBTPowerUserExtension)
 export class DBTPowerUserExtension implements Disposable {
   static DBT_SQL_SELECTOR = [
@@ -85,74 +80,10 @@ export class DBTPowerUserExtension implements Disposable {
     }
   }
 
-  async showWalkthrough() {
-    const answer = await window.showInformationMessage(
-      `Thanks for installing dbt Power User. Do you need help setting up the extension?`,
-      PromptAnswer.YES,
-      PromptAnswer.IGNORE,
-    );
-    commands.executeCommand(
-      "setContext",
-      "dbtPowerUser.showSetupWalkthrough",
-      true,
-    );
-    if (answer === PromptAnswer.YES) {
-      commands.executeCommand("dbtPowerUser.openSetupWalkthrough");
-    }
-  }
-
   async activate(context: ExtensionContext): Promise<void> {
     this.dbtProjectContainer.setContext(context);
+    this.dbtProjectContainer.initializeWalkthrough();
     await this.dbtProjectContainer.detectDBT();
     await this.dbtProjectContainer.initializeDBTProjects();
-
-    // show setup walkthrough if needed
-    const showSetupWalkthrough = context.globalState.get(
-      "showSetupWalkthrough",
-    );
-    if (showSetupWalkthrough === undefined || showSetupWalkthrough === true) {
-      this.showWalkthrough();
-    }
-
-    const allProjects = await this.dbtProjectContainer.getProjects();
-
-    commands.executeCommand(
-      "setContext",
-      "dbtPowerUser.projectCount",
-      allProjects.length,
-    );
-    if (allProjects.length === 1) {
-      this.dbtProjectContainer.setToWorkspaceState(
-        "dbtPowerUser.projectSelected",
-        {
-          label: allProjects[0].getProjectName(),
-          description: allProjects[0].projectRoot.fsPath,
-          uri: allProjects[0].projectRoot,
-        },
-      );
-      commands.executeCommand(
-        "setContext",
-        "dbtPowerUser.walkthroughProjectSelected",
-        true,
-      );
-    }
-    const existingAssociations = workspace
-      .getConfiguration("files")
-      .get<any>("associations", {});
-    let showFileAssociationsStep = false;
-    Object.entries({
-      "*.sql": ["jinja-sql", "sql"],
-      "*.yml": ["jinja-yaml", "yaml"],
-    }).forEach(([key, value]) => {
-      if (existingAssociations[key] === undefined) {
-        showFileAssociationsStep ||= true;
-      }
-      showFileAssociationsStep ||= !value.includes(existingAssociations[key]);
-    });
-    commands.executeCommand(
-      "setContext",
-      "dbtPowerUser.showFileAssociationStep",
-      showFileAssociationsStep,
-    );
   }
 }
