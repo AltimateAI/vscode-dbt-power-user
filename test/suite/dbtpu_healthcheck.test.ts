46af90d3f00b6b73889170ee636265b317456069
Shardul Sardesai <shardul@altimate.ai>
2023-11-15 12:50:06 -0800

    remove node-fetch, clean up imports

--- test/suite/dbtpu_healthcheck.test.ts
+++ test/suite/dbtpu_healthcheck.test.ts
@@ -3,11 +3,8 @@ import { waitForDebugger } from "inspector";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from "vscode";
-import { DBTPowerUserExtension } from "../../src/dbtPowerUserExtension";
-import { container } from "../../src/inversify.config";
-import * as dbtpu from "../../src/extension";
 import { DBTProjectContainer } from "../../src/manifest/dbtProjectContainer";
+import { container } from "../../src/inversify.config";
 
 suite("Extension setup healthcheck Suite", () => {
   test("Can verify extension init", async () => {
