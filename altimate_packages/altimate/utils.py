f712424b31604beb6046d8fe9ea18ee89d206818
suryaiyer95 <surya@altimate.ai>
2023-12-05 18:53:34 -0800

    fix: positions

--- altimate_packages/altimate/utils.py
+++ altimate_packages/altimate/utils.py
@@ -3,18 +3,48 @@ from sqlglot.optimizer.qualify import qualify
 from sqlglot.executor import execute
 
 
-def _build_message(error: dict):
-    message = ""
-    if error["description"]:
-        message += error["description"]
-    if error["line"]:
-        message += f" on line {error['line']}"
-    if error["col"]:
-        message += f", column {error['col']}"
-    if error["start_context"]:
-        message += f"\n{error['start_context']}"
-    return message
+def get_str_position(str, row, col):
+    """
+    Get the position of a string in a string
+    """
+    lines = str.split("\n")
+    position = 0
+    for i in range(row - 1):
+        position += len(lines[i]) + 1
+    position += col
+    return position
 
+
+def get_line_and_column_from_position(sql: str, position: int):
+    """
+    Get row and column from position in a string
+    """
+    lines = sql.split("\n")
+    row = 1
+    col = 1
+    for line in lines:
+        if position <= len(line):
+            col = position + 1
+            break
+        else:
+            position -= len(line) + 1
+            row += 1
+    return row, col
+
+
+def _build_message(sql: str, error: dict):
+    len_highlight = len(error["highlight"])
+    len_prefix = len(error["start_context"])
+    end_position = get_str_position(
+        sql, error["row"], error["col"]
+    )
+    start_position = end_position - len_highlight  - len_prefix
+    row, col = get_line_and_column_from_position(sql, start_position)
+    return {
+        "description": error["description"],
+        "start_position": [row, col],
+        "end_position":  [error["row"], error["col"]],
+    }
 
 def sql_parse_errors(sql: str, dialect: str):
     errors = []
@@ -22,7 +52,7 @@ def sql_parse_errors(sql: str, dialect: str):
         sqlglot.transpile(sql, read=dialect)
     except sqlglot.errors.ParseError as e:
         for error in e.errors:
-            errors.append(_build_message(error))
+            errors.append(_build_message(sql, error))
     return errors
 
 
@@ -36,7 +66,11 @@ def validate_tables_and_columns(
     try:
         qualify(parsed_sql, dialect=dialect, schema=schemas)
     except sqlglot.errors.OptimizeError as e:
-        return [str(e)]
+        return [
+            {
+                "description": str(e),
+            }
+        ]
 
     return None
 
@@ -64,5 +98,14 @@ def sql_execute_errors(
             tables=tables,
         )
     except sqlglot.errors.ExecuteError as e:
-        return [str(e)]
+        return [
+            {
+                "description": str(e),
+            }
+        ]
     return None
+
+
+if __name__ == "__main__":
+    sql = """SELECT1 * FROM table1"""
+    e = sql_parse_errors(sql, "bigquery")
