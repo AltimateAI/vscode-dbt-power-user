6f6436f3d44883f3ed205437b800939c8d08ec8c
Aditya <aditya.gajbhiye.20@gmail.com>
2023-12-11 14:42:32 +0000

    fix: hide sync with button for ephemeral

--- new_lineage_panel/src/TableDetails.tsx
+++ new_lineage_panel/src/TableDetails.tsx
@@ -119,23 +119,25 @@ const ColumnSection: FunctionComponent<{
     <div className={classNames(styles.card, "flex-grow column-section")}>
       <div className="d-flex flex-column gap-sm h-100 p-2">
         <div className="d-flex align-items-center gap-xs">
-          <div className="fs-5 fw-semibold">Columns</div>          
+          <div className="fs-5 fw-semibold">Columns</div>
           <div className="spacer" />
-          <Button
-            size="sm"
-            color="primary"
-            onClick={() => {
-              if (!selectedTable) {
-                return;
-              }
-              getColumns(selectedTable, true).then((_data) => {
-                setData(_data);
-                setFilteredColumn(_data.columns);
-              });
-            }}
-          >
-            Sync with DB
-          </Button>
+          {selectedTable?.materialization !== "ephemeral" && (
+            <Button
+              size="sm"
+              color="primary"
+              onClick={() => {
+                if (!selectedTable) {
+                  return;
+                }
+                getColumns(selectedTable, true).then((_data) => {
+                  setData(_data);
+                  setFilteredColumn(_data.columns);
+                });
+              }}
+            >
+              Sync with DB
+            </Button>
+          )}
         </div>
         <CustomInput
           bsSize="sm"
@@ -148,12 +150,10 @@ const ColumnSection: FunctionComponent<{
             );
           }}
         />
-        <div className="d-flex align-items-center gap-xs">                             
-          <div className="fs-xxs">            
-          Select column for lineage
-          </div>
+        <div className="d-flex align-items-center gap-xs">
+          <div className="fs-xxs">Select column for lineage</div>
           <PreviewIcon />
-          <div className="spacer" /> 
+          <div className="spacer" />
           <div className="fs-xxs text-grey">
             {filteredColumn.length} columns
           </div>
