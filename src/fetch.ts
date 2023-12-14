0e873610e88b411c8966a82b53894c2caa5c14cb
Sarav <sarav.majestic@gmail.com>
2023-12-14 14:34:51 +0530

    chore: comments

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,4 +1,7 @@
 // @ts-nocheck
+
+// Since we are using node-fetch in commonjs for testing, we have to dynamically import the library
+// https://github.com/node-fetch/node-fetch/blob/HEAD/docs/v3-UPGRADE-GUIDE.md#converted-to-es-module
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -30,10 +30,6 @@ export function run(): Promise<void> {
   return new Promise((resolve, reject) => {
     glob(testSuite, { cwd: testsRoot })
       .then((files: any[]) => {
-        //   if (err) {
-        //     return reject(err);
-        //   }
-
         // Add files to the test suite
         files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
 
