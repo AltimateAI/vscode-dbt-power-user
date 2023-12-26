6711f1d8d82e5b5b64896b9428177fe0ef1951df
Sarav <sarav.majestic@gmail.com>
2023-12-26 13:53:34 +0530

    feat: defer to prod

--- src/statusbar/index.ts
+++ src/statusbar/index.ts
@@ -1,13 +1,18 @@
 import { Disposable } from "vscode";
 import { provideSingleton } from "../utils";
 import { VersionStatusBar } from "./versionStatusBar";
+import { DeferStatusBar } from "./deferStatusBar";
 
 @provideSingleton(StatusBars)
 export class StatusBars implements Disposable {
   private disposables: Disposable[] = [];
 
-  constructor(private dbtStatusBar: VersionStatusBar) {
+  constructor(
+    private dbtStatusBar: VersionStatusBar,
+    private deferStatusBar: DeferStatusBar,
+  ) {
     this.disposables.push(this.dbtStatusBar);
+    this.disposables.push(this.deferStatusBar);
   }
 
   dispose() {
