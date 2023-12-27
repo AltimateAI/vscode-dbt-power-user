a6fe3d5cd9e1a6ca4b8498365d68d46b31329e5f
Sarav <sarav.majestic@gmail.com>
2023-12-27 14:00:23 +0530

    chore: linting rules

--- webview_panels/src/App.tsx
+++ webview_panels/src/App.tsx
@@ -3,45 +3,41 @@ import { Routes, Route, Link } from "react-router-dom";
 import NoMatch from "./NoMatch";
 
 const DocumentationEditor = lazy(
-  () => import("./modules/documentationEditor/DocumentationEditor")
+  () => import("./modules/documentationEditor/DocumentationEditor"),
 );
 const Insights = lazy(() => import("./modules/insights/Insights"));
 
 const Loader = () => <div>Loading...</div>;
-const App = () => {
-  return (
-    <>
-      <div>
-        hello webview
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/">
-              <Route path="insights" element={<Insights />} />
-              <Route
-                path="documentation-editor"
-                element={<DocumentationEditor />}
-              />
+const App = (): JSX.Element => (
+  <div>
+    hello webview
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/">
+          <Route path="insights" element={<Insights />} />
+          <Route
+            path="documentation-editor"
+            element={<DocumentationEditor />}
+          />
 
-              <Route path="*" element={<NoMatch />} />
-            </Route>
-          </Routes>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/insights">insights</Link>
-              </li>
-              <li>
-                <Link to="/documentation-editor">documentation-editor</Link>
-              </li>
-              <li>
-                <Link to="/nothing-here">Nothing Here</Link>
-              </li>
-            </ul>
-          </nav>
-        </Suspense>
-      </div>
-    </>
-  );
-};
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/insights">insights</Link>
+          </li>
+          <li>
+            <Link to="/documentation-editor">documentation-editor</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+    </Suspense>
+  </div>
+);
 
 export default App;
--- webview_panels/src/NoMatch.tsx
+++ webview_panels/src/NoMatch.tsx
@@ -1,5 +1,3 @@
-const NoMatch = () => {
-  return <div>Panel not found!</div>;
-};
+const NoMatch = (): JSX.Element => <div>Panel not found!</div>;
 
 export default NoMatch;
