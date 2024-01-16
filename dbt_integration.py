1abc19152a97991d8d77aa71c14189589e20d2e8
Michiel De Smet <mdesmet@gmail.com>
2024-01-16 23:39:26 +0400

    Fix compile

--- dbt_integration.py
+++ dbt_integration.py
@@ -285,6 +285,7 @@ class DbtProject:
         self.adapter = self.get_adapter()
         self.adapter.connections.set_connection_name()
         self.config.adapter = self.adapter
+        self.parse_project()
 
     def parse_project(self) -> None:
         project_parser = ManifestLoader(
