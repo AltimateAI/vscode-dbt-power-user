3ca2fe5e8e1d9a3d3ce5a4c5516d4cbca0be8f63
Aditya <aditya.gajbhiye.20@gmail.com>
2024-01-26 19:57:20 +0000

    fix: added alert notif

--- webview_panels/src/modules/commonActionButtons/CommonActionButtons.tsx
+++ webview_panels/src/modules/commonActionButtons/CommonActionButtons.tsx
@@ -1,9 +1,32 @@
-import { FeedbackIcon } from "@assets/icons";
+import { FeedbackIcon, InfoCircleIcon } from "@assets/icons";
 import HelpButton from "@modules/documentationEditor/components/help/HelpButton";
 import DocGeneratorSettings from "@modules/documentationEditor/components/settings/DocGeneratorSettings";
 import { vscode } from "@modules/vscode";
-import { Button, Stack } from "@uicore";
+import { Alert, Button, Stack } from "@uicore";
 import { executeRequestInAsync } from "../app/requestExecutor";
+import { FC, useEffect, useState } from "react";
+
+const AutoCollapsingNotif: FC<{ text: string; delay: number }> = ({
+  text,
+  delay,
+}) => {
+  const [showInfo, setShowInfo] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setShowInfo(false), delay);
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  return (
+    <Alert
+      color="warning"
+      onClick={() => setShowInfo((prevSetinfo) => !prevSetinfo)}
+      className="d-flex align-items-center cursor-pointer gap-sm m-0 p-2"
+    >
+      <InfoCircleIcon />
+      {showInfo && text}
+    </Alert>
+  );
+};
 
 const CommonActionButtons = (): JSX.Element => {
   const showOldUx = () => {
@@ -18,7 +41,11 @@ const CommonActionButtons = (): JSX.Element => {
   };
 
   return (
-    <Stack>
+    <Stack className="align-items-center">
+      <AutoCollapsingNotif
+        text="New langauge support added! (Check Settings)"
+        delay={10000}
+      />
       <Button outline onClick={showOldUx}>
         Show legacy UX
       </Button>
