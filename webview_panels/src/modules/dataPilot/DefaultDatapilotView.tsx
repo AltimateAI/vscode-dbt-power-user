78af3264f818c847cccac4c03a1a38344b813a90
Sarav <sarav.majestic@gmail.com>
2024-01-30 18:57:10 +0530

    fix: font size

--- webview_panels/src/modules/dataPilot/DefaultDatapilotView.tsx
+++ webview_panels/src/modules/dataPilot/DefaultDatapilotView.tsx
@@ -31,18 +31,18 @@ const DefaultDatapilotView = () => {
 
   return (
     <Stack direction="column" className={classes.defaultView}>
-      <Stack>
+      <Stack style={{ alignItems: "end" }}>
         <AltimateIcon />
         <div>
-          <h1>Hello, there!</h1>
-          <p className="p3">
+          <h3>Hello, there!</h3>
+          <h6>
             How can I help you today? <YellowEyeIcon />
-          </p>
+          </h6>
         </div>
       </Stack>
       <section>
-        <h3>Accelerate Your Work</h3>
-        <p className="p3">
+        <h6>Accelerate Your Work</h6>
+        <p>
           Hi there, I am your DataPilot and I’m here to help you get things done
           faster. I can help with documentation, query explanation / debugging,
           and dbt model generations. Please check{" "}
@@ -51,7 +51,8 @@ const DefaultDatapilotView = () => {
           </a>{" "}
           to know more.
         </p>
-        <p className="p3">
+        <br />
+        <p>
           I am still learning, and not perfect. Please share your feedback, so I
           can help you even better.{" "}
         </p>
@@ -59,15 +60,11 @@ const DefaultDatapilotView = () => {
 
       {!isNewDocsPanelEnabled ? (
         <section>
-          <h3>Enable docs generation new UX</h3>
-          <p className="p3">
+          <h6>Enable docs generation new UX</h6>
+          <p>
             Please enable new UX for docs generation to enable this
             functionality &nbsp;
-            <Button
-              color="primary"
-              onClick={enableNewUx}
-              style={{ marginTop: "1rem" }}
-            >
+            <Button color="primary" onClick={enableNewUx}>
               Enable New UX
             </Button>
           </p>
