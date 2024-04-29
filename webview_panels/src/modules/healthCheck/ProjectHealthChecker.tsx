6d889e52631555ac0559650703fe1245ebf6a203
Aditya Gajbhiye <AdiGajbhiye@users.noreply.github.com>
2024-04-29 09:27:40 +0530

    fix: governance UI (#1074)
    * feat: added loader on project governance api call
    
    * fix: added scan everything
    
    ---------
    
    Co-authored-by: anandgupta42 <93243293+anandgupta42@users.noreply.github.com>

--- webview_panels/src/modules/healthCheck/ProjectHealthChecker.tsx
+++ webview_panels/src/modules/healthCheck/ProjectHealthChecker.tsx
@@ -24,6 +24,7 @@ import {
   FolderIcon,
   ArrowLeftIcon,
   RefreshIcon,
+  LoadingIcon,
 } from "@assets/icons";
 import { ProjectHealthcheck } from "./types";
 import { IssueList } from "./IssueList";
@@ -46,6 +47,7 @@ type AltimateConfigProps = { projectRoot: string } & ConfigOption;
 enum ConfigType {
   Manual,
   Saas,
+  All,
 }
 
 interface SaasConfigSelectorProps {
@@ -59,10 +61,13 @@ interface SaasConfigSelectorProps {
 
 const SaasConfigSelector = (props: SaasConfigSelectorProps) => {
   const [configs, setConfigs] = useState<DBTConfig[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const getConfigs = useCallback(async () => {
+    setIsLoading(true);
     const result = (await executeRequestInSync("getInsightConfigs", {})) as {
       items: DBTConfig[];
     };
+    setIsLoading(false);
     setConfigs(result.items);
   }, []);
 
@@ -126,7 +131,7 @@ const SaasConfigSelector = (props: SaasConfigSelectorProps) => {
                 void getConfigs();
               }}
             >
-              <RefreshIcon />
+              {isLoading ? <LoadingIcon /> : <RefreshIcon />}
             </Button>
             <div className={classes.accordionContainer + " w-100"}>
               <Accordion
@@ -168,6 +173,17 @@ const SaasConfigSelector = (props: SaasConfigSelectorProps) => {
             </div>
           </>
         )}
+      </div>
+      <div className="d-flex align-items-center gap-sm">
+        <Label check sm={2} style={{ whiteSpace: "nowrap" }}>
+          <Input
+            type="radio"
+            className="me-2"
+            checked={props.configType === ConfigType.All}
+            onClick={() => props.setConfigType(ConfigType.All)}
+          />
+          Scan everything
+        </Label>
       </div>
     </div>
   );
@@ -241,7 +257,7 @@ const ProjectHealthcheckInput = ({
   >([]);
   const [selectedProject, setSelectedProject] = useState("");
   const [selectedConfig, setSelectedConfig] = useState<DBTConfig | undefined>();
-  const [configType, setConfigType] = useState(ConfigType.Manual);
+  const [configType, setConfigType] = useState(ConfigType.All);
   const [configPath, setConfigPath] = useState("");
   const [requestInProgress, setRequestInProgress] = useState(false);
 
@@ -263,7 +279,8 @@ const ProjectHealthcheckInput = ({
   const isStartScanEnabled =
     !requestInProgress &&
     selectedProject &&
-    ((configType === ConfigType.Manual && configPath) ||
+    (configType === ConfigType.All ||
+      (configType === ConfigType.Manual && configPath) ||
       (configType === ConfigType.Saas && selectedConfig));
 
   return (
