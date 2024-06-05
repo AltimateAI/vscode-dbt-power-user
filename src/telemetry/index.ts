849eea3ab0a7b0f55bd821e61adbfe16a4c58d28
saravmajestic <sarav.majestic@gmail.com>
2024-06-05 12:17:01 +0530

    fix: add telemetry attribute for dbt-loom

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -4,9 +4,14 @@ import { provideSingleton } from "../utils";
 
 @provideSingleton(TelemetryService)
 export class TelemetryService implements vscode.Disposable {
+  private customAttributes: { [key: string]: string } = {};
   private telemetryReporter: TelemetryReporter = new TelemetryReporter(
     "50598369-dd83-4f9a-9a65-ca1fa6f1785c",
   );
+
+  setTelemetryCustomAttribute(key: string, value: string) {
+    this.customAttributes[key] = value;
+  }
 
   sendTelemetryEvent(
     eventName: string,
@@ -28,6 +33,7 @@ export class TelemetryService implements vscode.Disposable {
           .get<boolean>("isLocalMode", false)
           ? "true"
           : "false",
+        ...this.customAttributes,
       },
       measurements,
     );
@@ -58,6 +64,7 @@ export class TelemetryService implements vscode.Disposable {
           error !== undefined && error instanceof Error
             ? error.stack
             : JSON.stringify(error),
+        ...this.customAttributes,
       },
       measurements,
     );
