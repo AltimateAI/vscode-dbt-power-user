88c33cba6e194045414cdaf43c0970e653ab0816
saravmajestic <sarav.majestic@gmail.com>
2024-06-06 12:23:47 +0530

    fix: legend UI

--- new_lineage_panel/src/components/LineageLegend.tsx
+++ new_lineage_panel/src/components/LineageLegend.tsx
@@ -1,14 +1,37 @@
-import { LENS_TYPE_COLOR } from "../utils"
+import { LENS_TYPE_COLOR } from "../utils";
 import styles from "../styles.module.scss";
+import { Button, Popover, PopoverBody } from "reactstrap";
+import { useState } from "react";
+import UpIcon from "../assets/icons/chevron-up.svg?react";
+import DownIcon from "../assets/icons/chevron-down.svg?react";
 
 const LineageLegend = () => {
-    return <div className={styles.column_legend}>
-    {Object.entries(LENS_TYPE_COLOR).map(([k, v]) => (
-      <div key={k}>
-        <div className={styles.dot}  style={{ backgroundColor: v }}/> {k}
-      </div>
-    ))}
-  </div>
-}
+  const [showLegend, setShowLegend] = useState(false);
 
-export default LineageLegend
+  const toggleLegend = () => setShowLegend(!showLegend);
+  return (
+    <>
+      <Button id="lineageLegend" className={styles.lineage_legend} type="button" onClick={toggleLegend}>
+        Legend 
+        {showLegend ? <DownIcon /> : <UpIcon />}
+      </Button>
+      <Popover
+        flip
+        target="lineageLegend"
+        isOpen={showLegend}
+        className={styles.column_legend}
+        placement="top"
+      >
+        <PopoverBody>
+          {Object.entries(LENS_TYPE_COLOR).map(([k, v]) => (
+            <div key={k}>
+              <div className={styles.dot} style={{ backgroundColor: v }} /> {k}
+            </div>
+          ))}
+        </PopoverBody>
+      </Popover>
+    </>
+  );
+};
+
+export default LineageLegend;
