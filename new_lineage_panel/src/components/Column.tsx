0e645e889c81793a307dd7d676e033f8d0ff891e
Aditya Gajbhiye <AdiGajbhiye@users.noreply.github.com>
2024-07-03 19:17:26 +0100

    fix: sql lineage (#1269)
    * fix: tab name for sql visualizer
    
    * fix: overllaping issue
    
    * fix: as per new design
    
    * fix: styling
    
    * fix: icons
    
    * fix: styling
    
    * fix: sql icons
    
    * fix: node icons
    
    ---------
    
    Co-authored-by: anandgupta42 <93243293+anandgupta42@users.noreply.github.com>

--- new_lineage_panel/src/components/Column.tsx
+++ new_lineage_panel/src/components/Column.tsx
@@ -5,6 +5,7 @@ import NumberIcon from "../assets/icons/number.svg?react";
 import ObjectIcon from "../assets/icons/object.svg?react";
 import DatetimeIcon from "../assets/icons/datetime.svg?react";
 import GeospatialIcon from "../assets/icons/geospatial.svg?react";
+
 import ModelIcon from "../assets/icons/model.svg?react";
 import SeedIcon from "../assets/icons/seed.svg?react";
 import SourceIcon from "../assets/icons/source.svg?react";
@@ -13,8 +14,28 @@ import AnalysisIcon from "../assets/icons/analysis.svg?react";
 import SnapshotIcon from "../assets/icons/snapshot.svg?react";
 import MetricsIcon from "../assets/icons/metrics.svg?react";
 import MacrosIcon from "../assets/icons/macros.svg?react";
+
+import CteDarkIcon from "../assets/icons/dbt_node_cte_dark.svg?react";
+import ModelDarkIcon from "../assets/icons/dbt_node_model_dark.svg?react";
+import SeedDarkIcon from "../assets/icons/dbt_node_seed_dark.svg?react";
+import SourceDarkIcon from "../assets/icons/dbt_node_source_dark.svg?react";
+import ExposureDarkIcon from "../assets/icons/dbt_node_exposure_dark.svg?react";
+// import AnalysisDarkIcon from "../assets/icons/dbt_node_analysis_dark.svg?react";
+import SnapshotDarkIcon from "../assets/icons/dbt_node_snapshot_dark.svg?react";
+import MetricsDarkIcon from "../assets/icons/dbt_node_metrics_dark.svg?react";
+
+import CteLightIcon from "../assets/icons/dbt_node_cte_light.svg?react";
+import ModelLightIcon from "../assets/icons/dbt_node_model_light.svg?react";
+import SeedLightIcon from "../assets/icons/dbt_node_seed_light.svg?react";
+import SourceLightIcon from "../assets/icons/dbt_node_source_light.svg?react";
+import ExposureLightIcon from "../assets/icons/dbt_node_exposure_light.svg?react";
+// import AnalysisLightIcon from "../assets/icons/dbt_node_analysis_light.svg?react";
+import SnapshotLightIcon from "../assets/icons/dbt_node_snapshot_light.svg?react";
+import MetricsLightIcon from "../assets/icons/dbt_node_metrics_light.svg?react";
+
 import styles from "../styles.module.scss";
 import { UncontrolledTooltip } from "reactstrap";
+import { getDarkMode } from "../Lineage";
 
 export const ColumnDatatype: FunctionComponent<{
   datatype: string;
@@ -58,6 +79,40 @@ export const ColumnDatatype: FunctionComponent<{
   );
 };
 
+const NodeTypeDarkIcon: FunctionComponent<{ nodeType: string }> = ({
+  nodeType,
+}) => (
+  <div>
+    {nodeType === "seed" && <SeedDarkIcon />}
+    {nodeType === "model" && <ModelDarkIcon />}
+    {nodeType === "cte" && <CteDarkIcon />}
+    {nodeType === "source" && <SourceDarkIcon />}
+    {nodeType === "exposure" && <ExposureDarkIcon />}
+    {nodeType === "analysis" && <AnalysisIcon />}
+    {nodeType === "snapshot" && <SnapshotDarkIcon />}
+    {nodeType === "semantic_model" && <MetricsDarkIcon />}
+    {nodeType === "macros" && <MacrosIcon />}
+    {nodeType === "unknown" && <ModelDarkIcon />}
+  </div>
+);
+
+const NodeTypeLightIcon: FunctionComponent<{ nodeType: string }> = ({
+  nodeType,
+}) => (
+  <div>
+    {nodeType === "seed" && <SeedLightIcon />}
+    {nodeType === "model" && <ModelLightIcon />}
+    {nodeType === "cte" && <CteLightIcon />}
+    {nodeType === "source" && <SourceLightIcon />}
+    {nodeType === "exposure" && <ExposureLightIcon />}
+    {nodeType === "analysis" && <AnalysisIcon />}
+    {nodeType === "snapshot" && <SnapshotLightIcon />}
+    {nodeType === "semantic_model" && <MetricsLightIcon />}
+    {nodeType === "macros" && <MacrosIcon />}
+    {nodeType === "unknown" && <ModelLightIcon />}
+  </div>
+);
+
 export const NodeTypeIcon: FunctionComponent<{ nodeType: string }> = ({
   nodeType,
 }) => (
@@ -74,6 +129,17 @@ export const NodeTypeIcon: FunctionComponent<{ nodeType: string }> = ({
     {nodeType === "unknown" && <ModelIcon />}
   </div>
 );
+
+export const NodeTypeIconForSQL: FunctionComponent<{
+  nodeType: string;
+}> = ({ nodeType }) => {
+  const isDarkMode = getDarkMode();
+  return isDarkMode ? (
+    <NodeTypeDarkIcon nodeType={nodeType} />
+  ) : (
+    <NodeTypeLightIcon nodeType={nodeType} />
+  );
+};
 
 export const TableNodePill: FunctionComponent<{
   id: string;
@@ -113,5 +179,5 @@ export const NODE_TYPE_SHORTHAND = {
   macros: "SEM",
   analysis: "ANY",
   cte: "CTE",
-  unknown: "UNK"
+  unknown: "UNK",
 };
