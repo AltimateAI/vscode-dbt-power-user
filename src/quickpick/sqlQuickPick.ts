d4eeaf601302df74d733073abef8d6fc6d40f8f0
saravmajestic <sarav.majestic@gmail.com>
2024-07-10 11:48:26 +0530

    fix: beta text and icons

--- src/quickpick/sqlQuickPick.ts
+++ src/quickpick/sqlQuickPick.ts
@@ -77,7 +77,7 @@ export class DbtSQLAction {
           ),
           new SQLActionItem(
             "Visualize SQL",
-            new ThemeIcon("eye"),
+            new ThemeIcon("preview"),
             "Visualize the sql query",
             "dbtPowerUser.sqlLineage",
           ),
