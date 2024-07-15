5f7aafef4c9e305f041838c1602d8c4ec6fb5187
Aditya <aditya.gajbhiye.20@gmail.com>
2024-07-15 14:41:12 +0100

    chore: code refactor

--- new_lineage_panel/src/Modals.tsx
+++ new_lineage_panel/src/Modals.tsx
@@ -1,6 +1,6 @@
-import { Dispatch, SetStateAction, useContext, useMemo } from "react";
+import { useContext, useMemo } from "react";
 import { CodeBlock, ViewsTypeBadge } from "./components";
-import { LineageContext, ModalArgs, ViewsCodeModalArgs } from "./Lineage";
+import { LineageContext, ViewsCodeModalArgs } from "./Lineage";
 import { Modal, ModalBody } from "reactstrap";
 import styles from "./styles.module.scss";
 import { HeaderSection } from "./TableDetails";
@@ -9,10 +9,8 @@ import { useReactFlow } from "reactflow";
 
 function ViewsCodeModal({
   viewsCodeModal: viewsCodeArgs,
-  setModalArgs,
 }: {
   viewsCodeModal: ViewsCodeModalArgs;
-  setModalArgs: Dispatch<SetStateAction<ModalArgs>>;
 }) {
   const flow = useReactFlow();
   const table = useMemo(() => {
@@ -34,9 +32,43 @@ function ViewsCodeModal({
   }, [viewsCodeArgs?.viewsCode]);
 
   return (
+    <div className="d-flex flex-column gap-sm">
+      {table && (
+        <HeaderSection nodeType={viewsCodeArgs.nodeType} table={table} />
+      )}
+      <div className="d-flex flex-column gap-xs">
+        <div className="text-dark-grey fs-xs">Column</div>
+        <div className={styles.model_views_type}>{viewsCodeArgs.column}</div>
+      </div>
+      <div className="d-flex flex-column gap-xs">
+        <div className="text-dark-grey fs-xs">Type</div>
+        <div className={styles.model_views_type}>
+          <ViewsTypeBadge viewsType={viewsCodeArgs.viewsType} />
+          {viewsCodeArgs.viewsType}
+        </div>
+      </div>
+      {viewsCodesFlat.length > 0 && (
+        <div className="d-flex flex-column gap-xs">
+          <div className="text-dark-grey fs-xs">List of transformations</div>
+          {viewsCodesFlat.map((code) => (
+            <CodeBlock key={code} code={code} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+// function OpNodeModal() {
+//   return;
+// }
+
+export function LineageModal() {
+  const { modalArgs, setModalArgs } = useContext(LineageContext);
+  return (
     <Modal
       size="lg"
-      isOpen={Boolean(viewsCodeArgs)}
+      isOpen={modalArgs.type !== "none"}
       toggle={() => setModalArgs({ type: "none" })}
       centered
       unmountOnClose
@@ -50,49 +82,10 @@ function ViewsCodeModal({
         >
           <CloseIcon />
         </div>
-        <div className="d-flex flex-column gap-sm">
-          {table && (
-            <HeaderSection nodeType={viewsCodeArgs.nodeType} table={table} />
-          )}
-          <div className="d-flex flex-column gap-xs">
-            <div className="text-dark-grey fs-xs">Column</div>
-            <div className={styles.model_views_type}>
-              {viewsCodeArgs.column}
-            </div>
-          </div>
-          <div className="d-flex flex-column gap-xs">
-            <div className="text-dark-grey fs-xs">Type</div>
-            <div className={styles.model_views_type}>
-              <ViewsTypeBadge viewsType={viewsCodeArgs.viewsType} />
-              {viewsCodeArgs.viewsType}
-            </div>
-          </div>
-          {viewsCodesFlat.length > 0 && (
-            <div className="d-flex flex-column gap-xs">
-              <div className="text-dark-grey fs-xs">
-                List of transformations
-              </div>
-              {viewsCodesFlat.map((code) => (
-                <CodeBlock key={code} code={code} />
-              ))}
-            </div>
-          )}
-        </div>
+        {modalArgs.type === "views_code" && (
+          <ViewsCodeModal viewsCodeModal={modalArgs.args} />
+        )}
       </ModalBody>
     </Modal>
   );
-}
-
-export function LineageModal() {
-  const { modalArgs, setModalArgs } = useContext(LineageContext);
-  if (modalArgs.type === "none") return null;
-  if (modalArgs.type === "views_code") {
-    return (
-      <ViewsCodeModal
-        setModalArgs={setModalArgs}
-        viewsCodeModal={modalArgs.args}
-      />
-    );
-  }
-  return null;
 }
