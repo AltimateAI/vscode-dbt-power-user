6e1348a833fdba42dcc21e923def026a162ffeeb
Vipul Rao <svipulrao98@gmail.com>
2024-08-08 17:01:35 +0530

    update logic

--- webview_panels/src/uiCore/components/button/Button.tsx
+++ webview_panels/src/uiCore/components/button/Button.tsx
@@ -3,14 +3,20 @@ import { Button as ReactStrapButton, ButtonProps } from "reactstrap";
 import Tooltip from "../tooltip/Tooltip";
 
 export const Button = (props: ButtonProps): JSX.Element => {
-  const defaultSHowText = props?.showtext ? true : false;
-  const isIconPresrent = props?.icon ? true : false;
-  const [showButtonText, setShowButtonText] = useState<boolean>(
-    isIconPresrent && !defaultSHowText ? false : true,
-  );
+  const defaultShowText = props?.showtext ? true : false;
+  const isIconPresent = props?.icon ? true : false;
+
+  // Set the initial state based on whether the text should be shown by default
+  const [showButtonText, setShowButtonText] =
+    useState<boolean>(defaultShowText);
+
   const mouseHoverAction = (showBtnText: boolean) => {
-    if (isIconPresrent && !defaultSHowText) setShowButtonText(showBtnText);
+    // Only toggle the text visibility if `showtext` is false
+    if (isIconPresent && !defaultShowText) {
+      setShowButtonText(showBtnText);
+    }
   };
+
   return (
     <Tooltip title={props.title}>
       <ReactStrapButton
@@ -18,7 +24,8 @@ export const Button = (props: ButtonProps): JSX.Element => {
         onMouseEnter={() => mouseHoverAction(true)}
         onMouseLeave={() => mouseHoverAction(false)}
       >
-        {isIconPresrent && props?.icon} {showButtonText && props.buttonText}
+        {isIconPresent && props?.icon}
+        {showButtonText && props.buttonText}
       </ReactStrapButton>
     </Tooltip>
   );
