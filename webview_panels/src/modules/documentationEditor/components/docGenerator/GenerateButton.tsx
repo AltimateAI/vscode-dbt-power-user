fff41b527878d3a5e77e1bb15737602f722094b2
Vipul Rao <svipulrao98@gmail.com>
2024-08-08 17:47:42 +0530

    add show text prop

--- webview_panels/src/modules/documentationEditor/components/docGenerator/GenerateButton.tsx
+++ webview_panels/src/modules/documentationEditor/components/docGenerator/GenerateButton.tsx
@@ -45,6 +45,7 @@ const GenerateButton = ({
       onClick={onSubmit}
       title={`Generate description for ${entityName}`}
       buttontext={getButtonText()}
+      showtext={true}
     />
   );
 };
