bdcb8fdf42a42166bf2382a68578ab1930f1211c
saravmajestic <sarav.majestic@gmail.com>
2024-09-18 09:46:48 +0530

    fix: notebook issues

--- src/code_lens_provider/index.ts
+++ src/code_lens_provider/index.ts
@@ -4,39 +4,44 @@ import { provideSingleton } from "../utils";
 import { SourceModelCreationCodeLensProvider } from "./sourceModelCreationCodeLensProvider";
 import { VirtualSqlCodeLensProvider } from "./virtualSqlCodeLensProvider";
 import { DocumentationCodeLensProvider } from "./documentationCodeLensProvider";
+import { DBTProjectContainer } from "../manifest/dbtProjectContainer";
 
 @provideSingleton(CodeLensProviders)
 export class CodeLensProviders implements Disposable {
   private disposables: Disposable[] = [];
   constructor(
+    private dbtProjectContainer: DBTProjectContainer,
     private sourceModelCreationCodeLensProvider: SourceModelCreationCodeLensProvider,
     private virtualSqlCodeLensProvider: VirtualSqlCodeLensProvider,
     private documentationCodeLensProvider: DocumentationCodeLensProvider,
   ) {
-    this.disposables.push(
-      languages.registerCodeLensProvider(
-        DBTPowerUserExtension.DBT_YAML_SELECTOR,
-        this.sourceModelCreationCodeLensProvider,
-      ),
-    );
-    this.disposables.push(
-      languages.registerCodeLensProvider(
-        DBTPowerUserExtension.DBT_SQL_SELECTOR,
-        this.virtualSqlCodeLensProvider,
-      ),
-    );
-    this.disposables.push(
-      languages.registerCodeLensProvider(
-        DBTPowerUserExtension.DBT_YAML_SELECTOR,
-        this.documentationCodeLensProvider,
-      ),
-    );
-    this.disposables.push(
-      languages.registerCodeLensProvider(
-        DBTPowerUserExtension.DBT_SQL_SELECTOR,
-        this.documentationCodeLensProvider,
-      ),
-    );
+    // Add codelens after projects are initialized to avoid race conditions in executing notebook cells
+    this.dbtProjectContainer.onDBTProjectsInitialization(() => {
+      this.disposables.push(
+        languages.registerCodeLensProvider(
+          DBTPowerUserExtension.DBT_YAML_SELECTOR,
+          this.sourceModelCreationCodeLensProvider,
+        ),
+      );
+      this.disposables.push(
+        languages.registerCodeLensProvider(
+          DBTPowerUserExtension.DBT_SQL_SELECTOR,
+          this.virtualSqlCodeLensProvider,
+        ),
+      );
+      this.disposables.push(
+        languages.registerCodeLensProvider(
+          DBTPowerUserExtension.DBT_YAML_SELECTOR,
+          this.documentationCodeLensProvider,
+        ),
+      );
+      this.disposables.push(
+        languages.registerCodeLensProvider(
+          DBTPowerUserExtension.DBT_SQL_SELECTOR,
+          this.documentationCodeLensProvider,
+        ),
+      );
+    });
   }
 
   dispose() {
