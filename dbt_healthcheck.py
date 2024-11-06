fbb67a2ff3de9769b5b697545f3984dbe6df8dcd
Michiel De Smet <mdesmet@gmail.com>
2024-11-06 11:03:25 +0800

    Support sql check

--- dbt_healthcheck.py
+++ dbt_healthcheck.py
@@ -7,20 +7,18 @@ def project_healthcheck(
     manifest_path, catalog_path=None, config_path=None, config=None
 ):
     try:
-        import logging
-        import json
-
-        from datapilot.config.config import load_config
-        from datapilot.core.platforms.dbt.utils import load_catalog
-        from datapilot.core.platforms.dbt.utils import load_manifest
-        from datapilot.core.platforms.dbt.constants import MODEL
-        from datapilot.core.platforms.dbt.executor import DBTInsightGenerator
-
         ALTIMATE_PACKAGE_PATH = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "altimate_packages"
         )
         with add_path(ALTIMATE_PACKAGE_PATH):
+            import logging
+            import json
 
+            from datapilot.config.config import load_config
+            from datapilot.core.platforms.dbt.utils import load_catalog
+            from datapilot.core.platforms.dbt.utils import load_manifest
+            from datapilot.core.platforms.dbt.constants import MODEL
+            from datapilot.core.platforms.dbt.executor import DBTInsightGenerator
             logging.basicConfig(level=logging.INFO)
             manifest = load_manifest(manifest_path)
             catalog = load_catalog(catalog_path) if catalog_path else None
