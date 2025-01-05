5ea6cfb7c720a27577668c3bd1b36cc29378edbe
anandgupta42 <anand@altimate.ai>
2025-01-04 18:41:01 -0800

    fixed coverage reporting

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -2,8 +2,12 @@ import "reflect-metadata";
 import * as path from "path";
 import Mocha from "mocha";
 import { glob } from "glob";
+import { setupCoverage } from "./coverage";
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
+  // Initialize coverage
+  setupCoverage();
+
   // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
@@ -13,27 +17,23 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, ".");
 
-  return new Promise((resolve, reject) => {
-    glob("**/**.test.js", { cwd: testsRoot })
-      .then((files: string[]) => {
-        try {
-          // Add files to the test suite
-          files.forEach((file: string) =>
-            mocha.addFile(path.resolve(testsRoot, file)),
-          );
+  try {
+    const files = await glob("**/**.test.js", { cwd: testsRoot });
+    // Add files to the test suite
+    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-          // Run the mocha test
-          mocha.run((failures: number) => {
-            if (failures > 0) {
-              reject(new Error(`${failures} tests failed.`));
-            } else {
-              resolve();
-            }
-          });
-        } catch (err) {
-          reject(err);
+    return new Promise<void>((resolve, reject) => {
+      // Run the mocha test
+      mocha.run((failures: number) => {
+        if (failures > 0) {
+          reject(new Error(`${failures} tests failed.`));
+        } else {
+          resolve();
         }
-      })
-      .catch((err) => reject(err));
-  });
+      });
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
