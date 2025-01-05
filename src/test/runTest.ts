a57c83bf9cb4de8a95b11ddbbed40976485ac7e2
anandgupta42 <anand@altimate.ai>
2025-01-04 17:50:43 -0800

    fixing codecov reporting

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -7,9 +7,6 @@ async function main() {
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
     const workspaceFolder = path.resolve(__dirname, "../../test-workspace");
 
-    // Create coverage directory if it doesn't exist
-    const coverageDirectory = path.resolve(__dirname, "../../coverage");
-
     // Run tests with coverage enabled
     await runTests({
       extensionDevelopmentPath,
@@ -19,10 +16,6 @@ async function main() {
         "--enable-proposed-api=innoverio.vscode-dbt-power-user",
         "--install-extension=samuelcolvin.jinjahtml",
       ],
-      extensionTestsEnv: {
-        COVERAGE_DIR: coverageDirectory,
-        CODE_COVERAGE: "1",
-      },
     });
   } catch (err) {
     console.error("Failed to run tests", err);
