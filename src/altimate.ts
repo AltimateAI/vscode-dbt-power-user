1d33d801c98a2b6136a492dd8547c8421d5ecd22
Saravanan S <sarav.majestic@gmail.com>
2025-02-28 14:03:09 +0530

    revert: to dropdown with urls

--- src/altimate.ts
+++ src/altimate.ts
@@ -338,26 +338,11 @@ export interface ConversationGroup {
   conversations: Conversation[];
 }
 
-const getAltimateUrl = (): string => {
-  const altimateUrlInSettings = workspace
-    .getConfiguration("dbt")
-    .get<string>("altimateUrl");
-
-  if (altimateUrlInSettings) {
-    return altimateUrlInSettings;
-  }
-
-  const isEnterprise = workspace
-    .getConfiguration("dbt")
-    .get<boolean>("altimatePlan", false);
-  return isEnterprise
-    ? "https://api.getaltimate.com"
-    : "https://api.myaltimate.com";
-};
-
 @provideSingleton(AltimateRequest)
 export class AltimateRequest {
-  public static ALTIMATE_URL = getAltimateUrl();
+  public static ALTIMATE_URL = workspace
+    .getConfiguration("dbt")
+    .get<string>("altimateUrl", "https://api.myaltimate.com");
 
   constructor(
     private telemetry: TelemetryService,
