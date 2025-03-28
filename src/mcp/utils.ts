dcbd2c3691cab35f42760cb65285966eaa96efed
Saravanan S <sarav.majestic@gmail.com>
2025-03-28 13:39:39 +0530

    chore: add ide name in telemetry

--- src/mcp/utils.ts
+++ src/mcp/utils.ts
@@ -1,11 +1,10 @@
 import portfinder from "portfinder";
+import { env } from "vscode";
 
 export const findAvailablePort = async (): Promise<number> => {
   return await portfinder.getPortPromise({ port: 7700, stopPort: 7900 });
 };
 
 export const isCursor = () => {
-  return (
-    process.env.VSCODE_CWD?.includes("Cursor") || !!process.env.CURSOR_TRACE_ID
-  );
+  return env.appName === "Cursor";
 };
