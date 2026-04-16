ec1d945aa2b6990cc6252cb164de190d1a5f88fa
Tse Kit Yam <tsekityam@users.noreply.github.com>
2026-04-16 19:18:07 +0800

    Update src/commands/walkthroughCommands.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/commands/walkthroughCommands.ts
+++ src/commands/walkthroughCommands.ts
@@ -396,8 +396,7 @@ export class WalkthroughCommands {
         return "dbt-synapse";
       case "fabric":
         return "dbt-fabric";
-      case "risingwave":
-        return "dbt-risingwave";
+  const specialCases = ["trino", "athena", "postgres", "duckdb", "risingwave"];
     }
     throw new Error("Adapter is not supported" + adapter);
   }
